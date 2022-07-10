4e93d2ee801f51b3d79962768b39cd4b7ca2522e
ryanbourdais <ryanbourdais@outlook.com>
2022-07-10 18:59:13 -0500

    added lacunarity, persistence, octaves, + seeding

--- Assets/Scenes/Main/PerlinGenerator.cs
+++ Assets/Scenes/Main/PerlinGenerator.cs
@@ -2,9 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class PerlinGenerator : MonoBehaviour
 {
-    public static randomTerrain randomTerrain = new randomTerrain(20, 513, 513, 8f);
+    public int depth = 20;
+    public int width = 513;
+    public int height = 513;
+    public float scale = 8;
+    public float persistance = .5f;
+    public float lacunarity = 2f;
+    public int octaves =  4;
+    public float worldOffset = 0f;
+    public int seed = 0;
+
+    public static randomTerrain randomTerrain = new randomTerrain(20, 513, 513, 8f, 0.5f, 2f, 4, 0f, 0);
+
+    private void Awake()
+    {
+        randomTerrain.Depth = depth;
+        randomTerrain.Width = width;
+        randomTerrain.Height = height;
+        randomTerrain.Scale = scale;
+        randomTerrain.Persistance = persistance;
+        randomTerrain.Lacunarity = lacunarity;
+        randomTerrain.Octaves = octaves;
+        randomTerrain.WorldOffset = worldOffset;
+        randomTerrain.Seed = seed;
+    }
 
     public static int getDepth() 
     {
@@ -25,6 +51,26 @@ public class PerlinGenerator : MonoBehaviour
     {
         return randomTerrain.Scale;
     }
+    public static float getPersistance()
+    {
+        return randomTerrain.Persistance;
+    }
+    public static float getLacunarity()
+    {
+        return randomTerrain.Lacunarity;
+    }
+    public static int getOctaves()
+    {
+        return randomTerrain.Octaves;
+    }
+    public static float getWorldOffset()
+    {
+        return randomTerrain.WorldOffset;
+    }
+    public static int getSeed()
+    {
+        return randomTerrain.Seed;
+    }
 }
 public class randomTerrain
 {
@@ -32,12 +78,22 @@ public class randomTerrain
     public int Width { get; set; }
     public int Height { get; set; }
     public float Scale { get; set; }
+    public float Persistance { get; set; }
+    public float Lacunarity { get; set; }
+    public int Octaves { get; set; }
+    public float WorldOffset { get; set; }
+    public int Seed { get; set; }
 
-    public randomTerrain(int depth, int width, int height, float scale)
+    public randomTerrain(int depth, int width, int height, float scale, float persistance, float lacunarity, int octaves, float worldOffset, int seed)
     {
         Depth = depth;
         Width = width;
         Height = height;
         Scale = scale;
+        Persistance = persistance;
+        Lacunarity = lacunarity;
+        Octaves = octaves;
+        WorldOffset = worldOffset;
+        Seed = seed;
     }
 }
--- Assets/Scenes/Main/TerrainGenerator.cs
+++ Assets/Scenes/Main/TerrainGenerator.cs
@@ -7,39 +7,37 @@ public class TerrainGenerator : MonoBehaviour
 
     private int width;
     private int height;
-
+    private int octaves;
+    private float persistance;
+    private float lacunarity;
     private float scale;
+    private int seed;
 
+    private float worldOffset = 0;
     private float offsetZ = 0;
     private float offsetX = 0;
-    private float _offsetX;
-    private float _offsetY;
+    public float _offsetX = 0f;
+    public float _offsetZ = 0f;
     private float offset = 7.9844f;
+    private float newoffset = 0f;
     private Terrain terrain;
 
     void Start() 
     {
+        
         depth = PerlinGenerator.getDepth();
         width = PerlinGenerator.getWidth();
         height = PerlinGenerator.getHeight();
         scale = PerlinGenerator.getScale();
-
-        if(scale != 8)
-        {
-            offset = offset * scale / 8;
-        }
-
-        offsetZ = this.transform.position.z / 513;
-        offsetX = this.transform.position.x / 513;
+        persistance = PerlinGenerator.getPersistance();
+        lacunarity = PerlinGenerator.getLacunarity();
+        octaves = PerlinGenerator.getOctaves();
+        worldOffset = PerlinGenerator.getWorldOffset();
+        seed = PerlinGenerator.getSeed();
         
         terrain = GetComponent<Terrain>();
         terrain.terrainData = GenerateTerrain(terrain.terrainData);
     }
-
-private void Update()
-{
-     //terrain.terrainData = GenerateTerrain(terrain.terrainData);
-}
 
     TerrainData GenerateTerrain (TerrainData terrainData)
     {
@@ -55,22 +53,63 @@ private void Update()
     float [,] GenerateHeights ()
     {
         float[,] heights = new float[width, height];
+
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+
+        for(int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-100000, 100000) + worldOffset;
+            float offsetZ = prng.Next(-100000, 100000) + worldOffset;
+            octaveOffsets[i] = new Vector2(offsetX, offsetZ);
+        }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfWidth = width / 2f;
+        float halfHeight = height / 2f;
+
         for (int x = 0; x < width; x++)
         {
             for(int y = 0; y < height; y++)
             {
-                heights[x,y] = CalculateHeight(x,y);
+                {
+                    float amplitude = 1;
+                    float frequency = 1;
+                    float noiseHeight = 0;
+
+                    for(int i = 0; i < octaves; i++)
+                    {
+
+                    float xCoord = ((float)x - halfWidth )/ scale * frequency + octaveOffsets[i].x + _offsetX;
+                    float yCoord = ((float)y - halfHeight) / scale * frequency + octaveOffsets[i].y + _offsetZ;
+
+                    float perlinValue = Mathf.PerlinNoise(xCoord, yCoord) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                    }
+                    if(noiseHeight > maxNoiseHeight)
+                    {
+                        maxNoiseHeight = noiseHeight;
+                    }
+                    else if (noiseHeight < minNoiseHeight)
+                    {
+                        minNoiseHeight = noiseHeight;
+                    }
+                    heights[x,y] = noiseHeight;
+                }
+            }
+        }
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {   
+                heights[x,y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, heights[x,y]);
             }
         }
         return heights;
-    }
-    float CalculateHeight (int x, int y)
-    {
-        _offsetX = offsetZ * offset;
-        _offsetY = offsetX * offset;
-        float xCoord = (float)x / width * scale + _offsetX;
-        float yCoord = (float)y / height * scale + _offsetY;
-
-        return Mathf.PerlinNoise(xCoord, yCoord);
     }
 }
