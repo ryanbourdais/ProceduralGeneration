d47220d8723520af2f90d14e11bee214bf19a5cc
ryanbourdais <ryanbourdais@outlook.com>
2022-07-11 17:00:50 -0500

    Added mesh drawing

--- Assets/Scenes/Noise Map/MapDisplay.cs
+++ Assets/Scenes/Noise Map/MapDisplay.cs
@@ -5,6 +5,8 @@ public class MapDisplay : MonoBehaviour
 {
 
 	public Renderer textureRender;
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
 
 	public void DrawTexture(Texture2D texture) 
     {
@@ -12,4 +14,9 @@ public class MapDisplay : MonoBehaviour
 		textureRender.transform.localScale = new Vector3 (texture.width, 1, texture.height);
 	}
 	
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+    {
+        meshFilter.sharedMesh = meshData.CreateMesh();
+        meshRenderer.sharedMaterial.mainTexture = texture;
+    }
 }
